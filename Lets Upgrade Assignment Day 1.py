93f90dd078668ccd9c374b4525e399737d11aa3d
sutanudutta <74143207+sutanudutta@users.noreply.github.com>
2020-11-08 19:53:45 +0530

    Day 1 Assignment submitted

---
+++ Lets Upgrade Assignment Day 1.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Question 1:   Ans (B) Rainbow
+
+# Question 2:
+
+# In[1]:
+
+
+a="Lets Upgrade"
+
+
+# In[3]:
+
+
+b=a.upper()
+
+
+# In[4]:
+
+
+print(b)
+
+
+# Question 3
+
+# In[32]:
+
+
+CP,SP=input().split()
+
+
+# In[33]:
+
+
+if (SP>CP) :
+    print ("Profit")
+elif (CP>SP): 
+    print ("Loss")
+else :
+    print ("Neither")
+    
+
+
+# Question 4
+# 
+
+# In[1]:
+
+
+INR=input()
+
+
+# In[17]:
+
+
+EURO=int(INR)*80
+
+
+# In[19]:
+
+
+print("Input\tOutput")
+print(INR+"\t"+ str(EURO))
+
+
+# In[ ]:
+
+
+
+
